5bd60f24a0347d7ff6d03be51aa78df4e9c5fa42
ChasobaAlpacanist <chasobakun@gmail.com>
2018-06-22 10:31:01 +0900

    9th modified

--- 9th/ex09_2.py
+++ 9th/ex09_2.py
@@ -11,12 +11,24 @@ def f(n):
     if(n <= 1):
         return 1
     else:
-        return (n - 1) * (n - 1 + f(n - 2))
+        return n * (n - 1 + f(n - 2)) #g(n - 1)
 
+def g(n):
+    if(n == 0):
+        return 0
+    else:
+        if(n - 1 <= 1):
+            return n + 1
+        else:
+            return n + ((n - 1) * g(n - 2)) #f(n - 1)
 def main():
-    print('f(5) = ' + str(f(5)))
+    print('f(5) = ' + str(f(1)))
+    print('g(5) = ' + str(g(1)))
     print('f(2) = ' + str(f(2)))
+    print('g(2) = ' + str(g(2)))
     print('f(0) = ' + str(f(0)))
-    print('f(-1) = ' + str(f(-1)))
+    print('g(0) = ' + str(g(0)))
+    print('f(-1) = ' + str(f(3)))
+    print('g(-1) = ' + str(g(3)))
 
 main()
